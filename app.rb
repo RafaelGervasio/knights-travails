7f5666d75c53a66deb164ef13b90df8c8221854f
Rafael Gervasio <rafaelgaspargervasio@gmail.com>
2023-10-13 19:51:52 +0100

    Project is finished

--- app.rb
+++ app.rb
@@ -1,24 +1,20 @@
 class Board
-    attr_accessor :board, :possible_moves
+    attr_accessor :possible_moves
 
     def initialize 
-        @board = []
         @possible_moves = [[1, 2] , [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]]
     end
 
     def knight_moves(starting_node, end_node, queue = [starting_node])
-        path = []
-        until queue[0] == end_node
-            if queue[0].visited = false
+        until queue[0].x == end_node.x && queue[0].y == end_node.y
+            if queue[0].visited == false
                 queue[0].visited = true
-                path.push(queue[0])
             end
-            queue[0].shift()
-            generate_paths(queue[0]).map {|arr| queue.push(arr)} 
+            generate_paths(queue[0]).map {|arr| queue.push(Node.new(arr[0], arr[1], queue[0]))} 
+            queue.shift()
         end
-        path.push(queue[0])
-        path
-        
+        puts "You made it in #{get_path(queue[0]).length - 1} moves! Here's your path:"
+        puts "#{get_path(queue[0])}"
     end
 
     def generate_paths(node)
@@ -36,25 +32,32 @@ class Board
         legal_moves
     end
 
-
+    def get_path (node)
+        path = []
+        until node.ptr.nil?
+            path.unshift([node.x, node.y])
+            node = node.ptr
+        end
+        path.unshift([node.x, node.y])
+        path
+    end
 end
    
 class Node
-    attr_accessor :x, :y, :visited 
+    attr_accessor :x, :y, :ptr, :visited 
 
-    def initialize(x, y)
+    def initialize(x, y, ptr = nil)
         @x = x
         @y = y
+        @ptr = ptr
         @visited = false
     end
 end
 
 
+test_node = Node.new(0, 0)
+test_node_2 = Node.new(7, 7)
 
-# starting_node = Node.new(starting_node[0], starting_node[1])
-# end_node = Node.new(end_node[0], end_node[1])
 
-
-test_node = Node.new(3, 3)
 b = Board.new
-p b.generate_paths(test_node)
+b.knight_moves(test_node, test_node_2)
